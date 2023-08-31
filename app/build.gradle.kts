d945d35ce8f865feeb3632266bcd064cc6755e2f
Mostafa Mohamed <mostafamansour163@yahoo.com>
2023-08-31 18:46:22 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,6 @@ android {
 
 dependencies {
 
-  //    implementation("androidx.core:core-ktx:1.10.1")
-//    implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -69,7 +67,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
     implementation ("androidx.core:core-ktx:1.10.1")
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.9.0")
@@ -81,33 +78,19 @@ dependencies {
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-
-//    implementation ("com.google.accompanist:accompanist-swiperefresh:0.26.5-rc")
     implementation("androidx.compose.material:material:1.4.0-beta01")
     implementation ("com.google.accompanist:accompanist-swiperefresh:0.24.13-rc")
-
     implementation ("androidx.activity:activity-compose:1.7.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
     implementation ("androidx.compose.runtime:runtime-livedata:1.0.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-//    // Jetpack Compose
-//    implementation ("androidx.activity:activity-compose:1.4.0")
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
-//    implementation ("androidx.compose.runtime:runtime-livedata:1.0.4")
-
-
-//     Coroutines for asynchronous programming
-//    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
 
     //Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
