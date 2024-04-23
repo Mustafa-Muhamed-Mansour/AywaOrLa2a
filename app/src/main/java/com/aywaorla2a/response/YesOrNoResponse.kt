bb2c061a8f70f25400ab9f3b32272f8eaf348076
Mostafa Mohamed <mostafamansour163@yahoo.com>
2024-04-23 16:47:58 +0200

    Update YesOrNoResponse.kt

--- app/src/main/java/com/aywaorla2a/response/YesOrNoResponse.kt
+++ app/src/main/java/com/aywaorla2a/response/YesOrNoResponse.kt
@@ -4,11 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class YesOrNoResponse (
     @SerializedName("answer")
-//    @SerializedName("credit_card_number")
     val answer: String,
-//    val creditCardNumber: String,
     @SerializedName("image")
-//    @SerializedName("credit_card_expiry_date")
     val image: String
-//    val creditCardExpiryDate: String
 )
